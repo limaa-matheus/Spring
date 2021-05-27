7ee16be9e957378f85b118fb4f671380c27c3ca4
Matheus Lima <matheus_lima1997@hotmail.com>
2021-05-27 01:52:49 -0300

    Spring com maven

---
+++ curso-udemy/src/main/java/com/curso/CursoUdemyApplication.java
@@ -0,0 +1,14 @@
+package com.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApplication.class, args);
+		
+			}
+
+}
