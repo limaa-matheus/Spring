216c7ef7ec201a5a570b272896fc0762fffabd2b
Matheus Lima <matheus_lima1997@hotmail.com>
2021-06-01 18:28:09 -0300

    Database

---
+++ teste-database/src/main/java/com/curso/database/TesteDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.curso.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDatabaseApplication.class, args);
+	}
+
+}
---
+++ teste-database/src/main/java/com/curso/database/entities/Curso.java
@@ -0,0 +1,38 @@
+package com.curso.database.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Curso {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "nome_do_curso" , nullable = false)
+	private String nome;
+
+	public Curso() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
