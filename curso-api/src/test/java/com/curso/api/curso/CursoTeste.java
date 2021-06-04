5e8e4e4ac2ff71ad2e6be8e2d9cdfd6ec7359c92
Matheus Lima <matheus_lima1997@hotmail.com>
2021-06-04 18:10:48 -0300

    API

---
+++ curso-api/src/test/java/com/curso/api/curso/CursoTeste.java
@@ -0,0 +1,14 @@
+package com.curso.api.curso;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTeste {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
